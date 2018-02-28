f45b820780bd3a3225b373766cb9e5eb6e2add4d
Zate <zate75@gmail.com>
2018-02-28 09:43:58 -0600

    fixed issue in docker-entrypoint

--- theia-go-docker/docker-entrypoint.sh
+++ theia-go-docker/docker-entrypoint.sh
@@ -5,5 +5,5 @@ if [ "$1" != 'stuff' ]; then
 	exec "$@"
 fi
 
-cd $TDIR
-exec yarn theia start /home/project/go/src --hostname=0.0.0.0
+cd $TDIR/theia/examples/browser
+exec yarn run start /home/project/go/src --hostname=0.0.0.0
